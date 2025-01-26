2f395c6a8e7db246e9da3fee670f49cc8286320a
Talha Hasan Zia <talhahasanzia@hotmail.com>
2025-01-26 15:52:08 +0500

    docs

---
+++ docs/assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1737887982',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/tags/', '/categories/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
