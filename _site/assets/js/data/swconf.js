362878d2b596c62516a03c527b0ae1d9cfca35d6
Talha Hasan Zia <talhahasanzia@hotmail.com>
2025-02-18 19:33:26 +0500

    site build

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1739888539',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/tags/', '/categories/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1739889188',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/tags/', '/categories/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
